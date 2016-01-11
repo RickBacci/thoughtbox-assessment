7f6113362cbe3fc2ce1af77894ccccf29c262804
Rick Bacci <rickbacci@gmail.com>
2016-01-11 12:39:22 -0700

    remove nested routes

--- app/controllers/api/v1/links_controller.rb
+++
@@ -1,4 +0,0 @@
-class Api::V1::LinksController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
 
-
   resources :sessions, only: [:new, :create, :destroy]
-
-  resources :users, only: [:new, :create]
-
-  namespace :api do
-    namespace :v1, defaults: { format: :json } do
-      resources :links, only: [:index]
-    end
-  end
+  resources :users,    only: [:new, :create]
+  resources :links,    only: [:index, :create]
 
   delete '/logout', to: 'sessions#destroy'
 
-  root 'api/v1/links#index'
+  root 'links#index'
 end
