f6abb943fcc8cecfd85320670ec5c298a86e65a2
Rick Bacci <rickbacci@gmail.com>
2016-01-11 14:42:19 -0700

    add user can update link status spec

--- spec/features/user_can_update_link_status_spec.rb
+++ spec/features/user_can_update_link_status_spec.rb
@@ -2,21 +2,79 @@ require 'rails_helper'
 
 RSpec.feature "Link", type: :feature, js:true do
 
-  it 'status can be updated by user' do
-    user = User.create!(email: 'user@email.com', password: 'password')
-    link = user.links.create(title: 'Turing', url: 'http://www.turing.io')
+  it 'can be marked as read' do
+    user = User.create!(email: 'testuser@email.com', password: 'password')
+    link = user.links.create!(title: 'Turing', url: 'http://www.turing.io')
 
     visit root_path
 
     click_link_or_button "Login"
-    fill_in "Email", with: 'user@email.com'
+    fill_in "Email", with: 'testuser@email.com'
     fill_in 'Password', with: 'password'
     click_link_or_button "Login"
 
+    click_link_or_button "Mark as Read"
 
+    expect(page).to have_content("Mark as Unread")
+    expect(page).to have_css('tr.false')
+  end
+
+  it 'can be marked as unread' do
+    user = User.create!(email: 'testuser@email.com', password: 'password')
+    link = user.links.create!(title: 'Turing', url: 'http://www.turing.io')
+
+    visit root_path
+
+    click_link_or_button "Login"
+    fill_in "Email", with: 'testuser@email.com'
+    fill_in 'Password', with: 'password'
+    click_link_or_button "Login"
 
     click_link_or_button "Mark as Read"
 
-    expect(page).to have_css('tr.false')
+    expect(page).to have_content("Mark as Unread")
+    click_link_or_button "Mark as Unread"
+    expect(page).to have_content("Mark as Read")
+  end
+
+  it 'changes color to red when it is marked as read' do
+    user = User.create!(email: 'testuser@email.com', password: 'password')
+    link = user.links.create!(title: 'Turing', url: 'http://www.turing.io')
+
+    visit root_path
+
+    click_link_or_button "Login"
+    fill_in "Email", with: 'testuser@email.com'
+    fill_in 'Password', with: 'password'
+    click_link_or_button "Login"
+
+    click_link_or_button "Mark as Read"
+
+    expect(page).to have_css('tr.red')
+
+    expect(page).to have_content("Mark as Unread")
+  end
+
+  it 'changes color to black when it is marked as unread' do
+    user = User.create!(email: 'testuser@email.com', password: 'password')
+    link = user.links.create!(title: 'Turing', url: 'http://www.turing.io')
+
+    visit root_path
+
+    click_link_or_button "Login"
+    fill_in "Email", with: 'testuser@email.com'
+    fill_in 'Password', with: 'password'
+    click_link_or_button "Login"
+
+    click_link_or_button "Mark as Read"
+
+    expect(page).to have_css('tr.red')
+    expect(page).to have_content("Mark as Unread")
+
+    click_link_or_button "Mark as Unread"
+
+    expect(page).to have_css('tr.black')
+    expect(page).to have_content("Mark as Read")
   end
 end
+
