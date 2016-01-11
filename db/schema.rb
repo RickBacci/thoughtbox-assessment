ff648b008abafe312b4ebf12fd21dec1e65cf486
Rick Bacci <rickbacci@gmail.com>
2016-01-11 14:42:05 -0700

    add default to link status

--- db/schema.rb
+++ db/schema.rb
@@ -19,9 +19,9 @@ ActiveRecord::Schema.define(version: 20160111190313) do
   create_table "links", force: :cascade do |t|
     t.string   "url"
     t.string   "title"
-    t.string   "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "status",     default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.integer  "user_id"
   end
 
