aafced90c7d15f0ab7d03bfd75f01b3424bd5af6
Rick Bacci <rickbacci@gmail.com>
2016-01-11 12:56:15 -0700

    add uniqueness for links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,6 @@
 class Link < ActiveRecord::Base
   belongs_to :user
 
-  validates :url, :url => true, presence: true
+  validates :url, :url => true, presence: true, uniqueness: true
   validates :title, presence: true
 end
