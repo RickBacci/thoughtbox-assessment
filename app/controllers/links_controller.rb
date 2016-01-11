c90b771879eb679526649a1a3f4d3be288e9f494
Rick Bacci <rickbacci@gmail.com>
2016-01-11 15:00:35 -0700

    add links#edit and links#update
    - add edit form in view

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -25,6 +25,17 @@ class LinksController < ApplicationController
     @link = Link.find(params[:id])
   end
 
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(link_params)
+      flash[:success] = "Link was successfully updated!"
+      redirect_to links_path
+    else
+      flash[:failure] = "There was a problem updating your link!"
+      redirect_to :back
+    end
+  end
+
   def update_status
     link = current_user.links.find(params[:link][:id])
 
