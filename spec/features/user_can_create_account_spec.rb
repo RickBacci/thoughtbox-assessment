752050d3e3ff931ed0646632c5900c8c59ae5c15
Rick Bacci <rickbacci@gmail.com>
2016-01-11 09:59:08 -0700

    add spec for user create account

---
+++ spec/features/user_can_create_account_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "User", type: :feature do
+
+  it 'can create an account' do
+
+    visit api_v1_thoughts_path
+
+    click_link "Sign Up"
+    fill_in "Email", with: 'user@email.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Confirmation', with: 'password'
+    click_link_or_button "Create Account"
+
+    expect(page).to have_content('Welcome')
+  end
+
+end
+
